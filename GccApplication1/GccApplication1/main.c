4822a5e962fffca339e3049247e85b37b9294983
Ian LaForge <ian.laforge@maine.edu>
2013-10-29 17:00:38 -0400

    Testing Memory

--- GccApplication1/GccApplication1/main.c
+++ GccApplication1/GccApplication1/main.c
@@ -32,33 +32,46 @@ int main(void)
 	unsigned int baud = 9600;
 	int wise = 2; //how many steps to take
 	int adcval;
+	char address = 0;
+	float watts = 9000;
+	float azimuth = 45;
+	float elevation = 90;
 	_delay_ms(500);
 	USART_Init (baud);
-	ADC_Init();
+	//ADC_Init();
 	LCD_Init();
 	_delay_ms(100);
 	
 	transmitstring("Data Points:",12);
 	nextline();
-	while(1){
-		if (RED_BUTTON){
-			transmitstring(a[points],3);
-			points++;
-				nextline();
-				if(points>3){
-					 points = 0;
-				} 
-			_delay_ms(1000);
-			} else {}
-		if(BLACK_BUTTON){
-			clearlcd();
-			_delay_ms(5);
-			transmitstring("Running!",8);
-			_delay_ms(2000);
-			break;
-		}
-	}
-	ADC_ON();
+	
+	EEPROM_address(0,address);
+	EEPROM_write_datapoint(watts,elevation,azimuth);
+	
+	transmitstring("complete!",9);
+	
+	
+	
+	
+	//while(1){
+		//if (RED_BUTTON){
+			//transmitstring(a[points],3);
+			//points++;
+				//nextline();
+				//if(points>3){
+					 //points = 0;
+				//} 
+			//_delay_ms(1000);
+			//} else {}
+		//if(BLACK_BUTTON){
+			//clearlcd();
+			//_delay_ms(5);
+			//transmitstring("Running!",8);
+			//_delay_ms(2000);
+			//break;
+		//}
+	//}
+	/*ADC_ON();
 	while(1){
 		adcval = ADCH;
 		itoa(ADCH,buffer,10);
@@ -66,10 +79,10 @@ int main(void)
 		transmitstring(buffer,strlen(buffer));
 		_delay_ms(5000);
 		clearlcd();
-	}
+	}*/
 		
 	
-	transmitstring("tits",4);
+	//transmitstring("tits",4);
 
 		//for(i=0; i<51; i++){
 			//currentazimuth = rotate_relative_azimuth(currentazimuth, 2); //rotate 180 degrees 
