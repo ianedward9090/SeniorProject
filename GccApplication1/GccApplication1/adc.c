0f0949867efb37a10c073118cc26492600376bd3
Ian LaForge <ian.laforge@maine.edu>
2013-11-08 18:46:06 -0500

    Ready for C grade - calibrations CUTTING IT CLOSE THERE BUB

--- GccApplication1/GccApplication1/adc.c
+++ GccApplication1/GccApplication1/adc.c
@@ -7,7 +7,7 @@ void ADC_Init(void){
 	//ADCSRA |= (1 << ADPS2) | (1 << ADPS0); //prescaler of 32
 	ADCSRA |= (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0); //Prescaler of 128   8M/128 = 62.5k
 	ADMUX |= (1 << REFS0); //Sets Reference to AVCC
-	ADMUX |= (1 << ADLAR); //Turns the ADC basically into a 8 bit ADC from 10 BIT !!!!READ ADCH!!!!
+	//ADMUX |= (1 << ADLAR); //Turns the ADC basically into a 8 bit ADC from 10 BIT !!!!READ ADCH!!!!
 	//ADCSRA |= (1 << ADFR); //free running mode 
 }
 
@@ -24,6 +24,6 @@ ISR(ADC_vect){
 int ADC_READ(void){
 	ADCSRA |= (1<<ADSC);
 	while (ADCSRA & (1<<ADSC));
-	return ADCH;
+	return ADC;
 	
 	}
--- GccApplication1/GccApplication1/motor.c
+++ GccApplication1/GccApplication1/motor.c
@@ -62,7 +62,7 @@ unsigned int rotate_relative_azimuth(int current_state, int steps) {
 		current_state = next_state%4;
 		
 		// Delay 10ms between steps
-		_delay_ms(5);
+		_delay_ms(10);
 	}
 	//PORTD = 0;
 	return current_state;
@@ -91,7 +91,7 @@ unsigned int rotate_relative_elevation(int current_state, int steps) {
 		current_state = next_state%4;
 		
 		// Delay 10ms between steps
-		_delay_ms(50);
+		_delay_ms(10);
 	}
 	//PORTD = 0;
 	return current_state;
