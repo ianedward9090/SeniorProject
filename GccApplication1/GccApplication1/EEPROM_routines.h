41897b3b6183ae9339691fa62bf1e7e2145e7cb3
Ian LaForge <ian.laforge@maine.edu>
2013-12-10 16:48:38 -0500

    Removed some old files

--- GccApplication1/GccApplication1/EEPROM_routines.h
+++
@@ -1,9 +0,0 @@
-// **************************************************************
-// ******* 24C256 EEPROM_ROUTINES HEADER FILE ******** 		
-// **************************************************************
-
-unsigned char EEPROM_read( unsigned char, unsigned char, unsigned char );
-unsigned char EEPROM_write( unsigned char, unsigned char );
-unsigned char EEPROM_writePage( unsigned int );
-unsigned char EEPROM_readPage( unsigned int );
-unsigned char EEPROM_erase( void );
